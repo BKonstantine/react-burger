55bf0829544fd4a1ad95572ed5cc5b84fd34dd76
Kostya Buzunov <buzunov.constantine@yandex.ru>
2022-11-25 11:56:22 +0300

    feat: сделать базовую структуру BurgerIngredients

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,14 @@
 import AppHeader from "../AppHeader/AppHeader";
+import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
+import style from "./App.module.css";
 
 export default function App() {
   return (
     <>
       <AppHeader />
+      <main className={style.main}>
+        <BurgerIngredients />
+      </main>
     </>
   );
 }
