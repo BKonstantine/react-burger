b5eba0c669f20f01af50d276cd90a72a709d4cd6
Kostya Buzunov <buzunov.constantine@yandex.ru>
2022-11-28 14:50:27 +0300

    fix: убрать лишний компонент

--- src/components/CustomButton/CustomButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function CustomButton(props) {
-  return (
-    <button className={`${props.style} pl-5 pr-5 pt-4 pb-4`} type="button">
-      {props.children}
-    </button>
-  );
-}
