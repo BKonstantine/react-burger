0c6226923251ddda6fd952ede324692ff043a6e7
Kostya Buzunov <buzunov.constantine@yandex.ru>
2022-11-30 15:52:57 +0300

    fix: исправить AppHeader

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -14,7 +14,7 @@ export default function AppHeader() {
         <nav className={style.nav}>
           <a
             href="#"
-            className={`text text_type_main-default ${style.button_active}`}
+            className={`text text_type_main-default ${style.button} ${style.button_active}`}
           >
             <BurgerIcon type="primary" />
             Конструктор
@@ -23,15 +23,17 @@ export default function AppHeader() {
             <ListIcon type="secondary" />
             Лента заказов
           </a>
+          <div className={style.logo}>
+            <Logo />
+          </div>
+          <a
+            href="#"
+            className={`text text_type_main-default ${style.button} ${style.button_profile}`}
+          >
+            <ProfileIcon type="secondary" />
+            Личный кабинет
+          </a>
         </nav>
-        <Logo />
-        <a
-          href="#"
-          className={`text text_type_main-default ${style.button} ${style.button_profile}`}
-        >
-          <ProfileIcon type="secondary" />
-          Личный кабинет
-        </a>
       </div>
     </header>
   );
