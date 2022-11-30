cf526c550d3477137eab8846bea939ba1b9b2d17
Kostya Buzunov <buzunov.constantine@yandex.ru>
2022-11-30 12:09:38 +0300

    feat: сделать компоненты для модальных окон

---
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -0,0 +1,5 @@
+import style from "./ingredient-details.module.css";
+
+export default function IngredientDetails(props) {
+  return <></>;
+}
---
+++ src/components/order-details/order-details.jsx
@@ -0,0 +1,5 @@
+import style from "./order-details.module.css";
+
+export default function OrderDetails(props) {
+  return <></>;
+}
