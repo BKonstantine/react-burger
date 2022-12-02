b195ba5a0a9a96d89a08fc55b6f58665a6588720
Kostya Buzunov <buzunov.constantine@yandex.ru>
2022-12-02 11:30:37 +0300

    fix: испралвена проверка типов

--- src/components/burger-ingredients-list/burger-ingredients-list.jsx
+++ src/components/burger-ingredients-list/burger-ingredients-list.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "../card/card";
 import style from "./burger-ingredients-list.module.css";
 import PropTypes from "prop-types";
+import cardPropTypes from "../../utils/prop-types";
 
 const BurgerIngredientsList = React.forwardRef((props, ref) => {
   return (
@@ -22,7 +23,7 @@ BurgerIngredientsList.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
-  ingredients: PropTypes.arrayOf(PropTypes.object),
+  ingredients: PropTypes.arrayOf(cardPropTypes),
 };
 
 export default BurgerIngredientsList;
