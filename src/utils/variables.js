ef80a1cc59078a26ac643e00be2a269be6dc2e1b
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-01-30 12:22:13 +0300

    feat: добавить URL к эндпоинтам

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,5 +1,8 @@
 const BASE_API_URL = "https://norma.nomoreparties.space/api";
 const INGREDIENTS_URL = `${BASE_API_URL}/ingredients`;
 const ORDER_URL = `${BASE_API_URL}/orders`;
+const REGISTER_URL = `${BASE_API_URL}/auth/register`;
+const LOGIN_URL = `${BASE_API_URL}/auth/login`;
+const LOGOUT_URL = `${BASE_API_URL}/auth/logout `;
 
-export { INGREDIENTS_URL, ORDER_URL };
+export { INGREDIENTS_URL, ORDER_URL, REGISTER_URL, LOGIN_URL, LOGOUT_URL };
