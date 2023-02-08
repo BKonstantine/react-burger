8dd0959644fea0202d036b9bf60957dea1cb6e43
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-02-08 15:57:52 +0300

    feat: сделать разметку компонента order-counters

--- src/components/order-counters/order-counters.jsx
+++ src/components/order-counters/order-counters.jsx
@@ -1,5 +1,40 @@
 import style from "./order-counters.module.css";
 
 export default function OrderCounters() {
-  return <div className={style.container}></div>;
+  return (
+    <div className={style.container}>
+      <div className={style.container__orders}>
+        <div className={style.container__done}>
+          <p className="text text_type_main-medium">Готовы:</p>
+          <ul className={style.orders}>
+            <li className={`text text_type_digits-default ${style.order}`}>
+              034533
+            </li>
+            <li className={`text text_type_digits-default ${style.order}`}>
+              034533
+            </li>
+            <li className={`text text_type_digits-default ${style.order}`}>
+              034533
+            </li>
+          </ul>
+        </div>
+        <div className={style.container__work}>
+          <p className="text text_type_main-medium">В работе:</p>
+          <ul className={style.orders}>
+            <li className={`text text_type_digits-default`}>034533</li>
+            <li className={`text text_type_digits-default`}>034533</li>
+            <li className={`text text_type_digits-default`}>034533</li>
+          </ul>
+        </div>
+      </div>
+      <div className={style.container__digit}>
+        <p className="text text_type_main-medium">Выполнено за все время:</p>
+        <p className={`text text_type_digits-large ${style.digit}`}>28752</p>
+      </div>
+      <div className={style.container__digit}>
+        <p className="text text_type_main-medium">Выполнено за сегодня:</p>
+        <p className={`text text_type_digits-large ${style.digit}`}>138</p>
+      </div>
+    </div>
+  );
 }
