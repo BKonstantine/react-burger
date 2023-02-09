8c40af9d55c6aee1e44238bf6b05c8c8aa65975f
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-02-09 22:39:04 +0300

    feat: настроить отображение списка заказов

--- src/components/order-counters/order-counters.jsx
+++ src/components/order-counters/order-counters.jsx
@@ -1,39 +1,52 @@
+import { v4 as uuidv4 } from "uuid";
 import style from "./order-counters.module.css";
 
-export default function OrderCounters() {
+export default function OrderCounters({
+  total,
+  totalToday,
+  doneList,
+  workList,
+}) {
   return (
     <div className={style.container}>
       <div className={style.container__orders}>
         <div className={style.container__done}>
           <p className="text text_type_main-medium">Готовы:</p>
           <ul className={style.orders}>
-            <li className={`text text_type_digits-default ${style.order}`}>
-              034533
-            </li>
-            <li className={`text text_type_digits-default ${style.order}`}>
-              034533
-            </li>
-            <li className={`text text_type_digits-default ${style.order}`}>
-              034533
-            </li>
+            {doneList.map((item) => {
+              return (
+                <li
+                  key={uuidv4()}
+                  className={`text text_type_digits-default ${style.order}`}
+                >
+                  {item}
+                </li>
+              );
+            })}
           </ul>
         </div>
         <div className={style.container__work}>
           <p className="text text_type_main-medium">В работе:</p>
           <ul className={style.orders}>
-            <li className={`text text_type_digits-default`}>034533</li>
-            <li className={`text text_type_digits-default`}>034533</li>
-            <li className={`text text_type_digits-default`}>034533</li>
+            {workList.map((item) => {
+              return (
+                <li key={uuidv4()} className={`text text_type_digits-default`}>
+                  {item}
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
       <div className={style.container__digit}>
         <p className="text text_type_main-medium">Выполнено за все время:</p>
-        <p className={`text text_type_digits-large ${style.digit}`}>28752</p>
+        <p className={`text text_type_digits-large ${style.digit}`}>{total}</p>
       </div>
       <div className={style.container__digit}>
         <p className="text text_type_main-medium">Выполнено за сегодня:</p>
-        <p className={`text text_type_digits-large ${style.digit}`}>138</p>
+        <p className={`text text_type_digits-large ${style.digit}`}>
+          {totalToday}
+        </p>
       </div>
     </div>
   );
--- src/pages/feed-page/feed-page.jsx
+++ src/pages/feed-page/feed-page.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AppHeader from "../../components/app-header/app-header";
 import OrderFeedList from "../../components/order-feed-list/order-feed-list";
@@ -11,15 +11,34 @@ import style from "./feed-page.module.css";
 
 export default function FeedPage() {
   const dispatch = useDispatch();
-  let orders = useSelector((store) => store.socketReducer.orders);  
+  const { orders, total, totalToday } = useSelector(
+    (store) => store.socketReducer
+  );
+
+  const { doneList, workList } = useMemo(() => {
+    return orders.reduce(
+      (count, item) => {
+        // eslint-disable-next-line default-case
+        switch (item.status) {
+          case "done":
+            count.doneList.push(item.number);
+            break;
+          case "work":
+            count.workList.push(item.number);
+            break;
+        }
+        return count;
+      },
+      { doneList: [], workList: [] }
+    );
+  }, [orders]);
 
   useEffect(() => {
     dispatch(wsConnectionStart());
-
     return () => {
       dispatch(wsConnectionClosed());
     };
-  }, []);
+  }, []);  
 
   return (
     <>
@@ -28,7 +47,12 @@ export default function FeedPage() {
         <p className="text text_type_main-large pt-10 pb-5">Лента заказов</p>
         <div className={style.container}>
           <OrderFeedList orders={orders} isFeedList={false} />
-          <OrderCounters />
+          <OrderCounters
+            doneList={doneList}
+            workList={workList}
+            total={total}
+            totalToday={totalToday}
+          />
         </div>
       </main>
     </>
