dc6ff297c7b482b00a5faa732d5c089053b843f8
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-02-09 22:47:43 +0300

    feat: настроить рендер компонента user-order-page

--- src/pages/user-order-page/user-order-page.jsx
+++ src/pages/user-order-page/user-order-page.jsx
@@ -1,6 +1,27 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  wsConnectionStart,
+  wsConnectionClosed,
+} from "../../services/actions/socketAction";
 import OrderFeedList from "../../components/order-feed-list/order-feed-list";
 import style from "./user-order-page.module.css";
 
 export default function UserOrderPage() {
-  return <OrderFeedList isFeedList={true} listClassName={style.list}/>;
+  const dispatch = useDispatch();
+  const { orders } = useSelector((store) => store.socketReducer);
+  useEffect(() => {
+    dispatch(wsConnectionStart());
+    return () => {
+      dispatch(wsConnectionClosed());
+    };
+  }, []);
+
+  return (
+    <OrderFeedList
+      orders={orders}
+      isFeedList={true}
+      listClassName={style.list}
+    />
+  );
 }
