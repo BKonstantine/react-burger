8488ed720b49e4cd215ee2c71856694454e9d738
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-02-10 18:43:44 +0300

    feat: убрать AppHeader из order-page

--- src/pages/order-page/order-page.jsx
+++ src/pages/order-page/order-page.jsx
@@ -1,11 +1,9 @@
-import AppHeader from "../../components/app-header/app-header";
 import BurgerDetails from "../../components/burger-details/burger-details";
 import style from "./order-page.module.css";
 
 export default function OrderPage() {
   return (
-    <>
-      <AppHeader />
+    <>      
       <main className={style.main}>
         <BurgerDetails titleClassName={style.title}/>
       </main>
