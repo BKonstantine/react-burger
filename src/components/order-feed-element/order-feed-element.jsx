db6da919ce95aad8ba3c64e7cb728d5e2cd4ef51
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-02-10 15:56:57 +0300

    feat: настроить добавление текущего заказа в стор

--- src/components/order-feed-element/order-feed-element.jsx
+++ src/components/order-feed-element/order-feed-element.jsx
@@ -1,15 +1,23 @@
+import { useDispatch } from "react-redux";
 import cn from "classnames";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import OrderIngredientsList from "../order-ingredients-list/order-ingredients-list";
 import style from "./order-feed-element.module.css";
 import useOrder from "../../hooks/useOrder";
+import { SET_CURRENT_ORDER } from "../../services/actions/currentOrderAction";
 
 export default function OrderFeedElement({ isFeedList, order }) {
   const { orderIngredientsList, orderPrice, orderStatus, orderDate } =
     useOrder(order);
 
+  const dispatch = useDispatch();
+
+  function openModal() {
+    dispatch({ type: SET_CURRENT_ORDER, payload: order });
+  }
+
   return (
-    <li className={cn(style.container)}>
+    <li className={cn(style.container)} onClick={openModal}>
       <div className={style.container__order}>
         <p className="text text_type_digits-default">{`#${order.number}`}</p>
         <p className="text text_type_main-default text_color_inactive">
