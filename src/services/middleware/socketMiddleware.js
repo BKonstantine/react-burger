0713e99fb12cf86ac3710e6f7e0bb859c4ab9ca6
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-02-12 16:47:40 +0300

    feat: настроить таймер переподключения к сокету

--- src/services/middleware/socketMiddleware.js
+++ src/services/middleware/socketMiddleware.js
@@ -1,6 +1,8 @@
 export const socketMiddleware = (wsActions) => {
   return (store) => {
     let socket = null;
+    let reconnectTimer = 0;
+    let url = undefined;
 
     return (next) => (action) => {
       const { dispatch } = store;
@@ -9,32 +11,41 @@ export const socketMiddleware = (wsActions) => {
         wsActions;
 
       if (type === wsInit) {
-        socket = new WebSocket(payload);
+        url = payload;
+        socket = new WebSocket(url);
       } else if (type === onClose) {
         socket.close(1000, "CLOSE_NORMAL");
+        clearTimeout(reconnectTimer);
+        reconnectTimer = 0;
       }
 
       if (socket) {
         socket.onopen = (event) => {
-          console.log("Открыт");
+          console.log("Открыт", event);
           dispatch({ type: onOpen, payload: event });
         };
 
         socket.onerror = (event) => {
-          console.log("Ошибка");
+          console.log("Ошибка", event);
           dispatch({ type: onError, payload: event });
         };
 
         socket.onmessage = (event) => {
+          console.log("Сообщение", event);
           const { data } = event;
           const parsedData = JSON.parse(data);
-          console.log(parsedData);
           const { success, ...restParsedData } = parsedData;
           dispatch({ type: onMessage, payload: restParsedData });
         };
 
         socket.onclose = (event) => {
-          console.log("Закрыт");
+          console.log("Закрыт", event);
+          if (event.code !== 1000) {
+            reconnectTimer = window.setTimeout(() => {
+              dispatch({ type: wsInit, payload: url });
+            }, 10000);
+          }
+
           dispatch({ type: onClosed, payload: event });
         };
       }
