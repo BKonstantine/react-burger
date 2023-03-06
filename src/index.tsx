c1bc984be1101cf6bb1bf3dba33486a4e646c90b
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-06 10:17:18 +0300

    feat: сделать типизацию index.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./components/app/app";
-import { compose, createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { rootReducer } from "./services/reducers/index";
 import { socketMiddleware } from "./services/middleware/socketMiddleware";
 import {
-  WS_CONNECTION_START,  
+  WS_CONNECTION_START,
   WS_CONNECTION_SUCCESS,
   WS_CONNECTION_ERROR,
   WS_CONNECTION_CLOSE,
@@ -16,7 +17,7 @@ import {
 } from "./services/constants/index";
 
 const wsActions = {
-  wsInit: WS_CONNECTION_START,  
+  wsInit: WS_CONNECTION_START,
   onOpen: WS_CONNECTION_SUCCESS,
   onClose: WS_CONNECTION_CLOSE,
   onClosed: WS_CONNECTION_CLOSED,
@@ -24,18 +25,15 @@ const wsActions = {
   onMessage: WS_GET_MESSAGE,
 };
 
-const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const enhancer = composeEnhancers(
+const enhancer = composeWithDevTools(
   applyMiddleware(thunk, socketMiddleware(wsActions))
 );
 
 const store = createStore(rootReducer, enhancer);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <Provider store={store}>
     <BrowserRouter>
