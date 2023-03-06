1de80c45cab581fc830977e13a7c8989f83817bd
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-06 11:48:09 +0300

    feat: указать типы для burgerConstructorAction

--- src/services/types/data.ts
+++ src/services/types/data.ts
@@ -1,3 +1,87 @@
+import {
+  ADD_INGREDIENT,
+  SORT_INGREDIENTS,
+  DELETE_INGREDIENT,
+  RESET_INGREDIENT,
+  CHANGE_USER_DATA_FORM_SUBMIT,
+  CHANGE_USER_DATA_FORM_SUBMIT_FAILED,
+  CHANGE_USER_DATA_FORM_SUBMIT_SUCCESS,
+  FORGOT_PASSWORD_FORM_SET_VALUE,
+  FORGOT_PASSWORD_FORM_SUBMIT,
+  FORGOT_PASSWORD_FORM_SUBMIT_FAILED,
+  FORGOT_PASSWORD_FORM_SUBMIT_SUCCESS,
+  GET_INGREDIENTS_FAILED,
+  GET_INGREDIENTS_REQUEST,
+  GET_INGREDIENTS_SUCCESS,
+  GET_ORDER_FAILED,
+  GET_ORDER_REQUEST,
+  GET_ORDER_SUCCESS,
+  RESET_CURRENT_INGREDIENT,
+  RESET_ORDER,
+  RESET_PASSWORD_FORM_SET_VALUE,
+  RESET_PASSWORD_FORM_SUBMIT,
+  RESET_PASSWORD_FORM_SUBMIT_FAILED,
+  RESET_PASSWORD_FORM_SUBMIT_SUCCESS,
+  SET_CURRENT_INGREDIENT,
+  USER_ACCESS_ALLOWED,
+  USER_ACCESS_DENIED,
+  USER_LOGIN_FORM_SET_VALUE,
+  USER_LOGIN_FORM_SUBMIT,
+  USER_LOGIN_FORM_SUBMIT_FAILED,
+  USER_LOGIN_FORM_SUBMIT_SUCCESS,
+  USER_REGISTER_FORM_SET_VALUE,
+  USER_REGISTER_FORM_SUBMIT,
+  USER_REGISTER_FORM_SUBMIT_FAILED,
+  USER_REGISTER_FORM_SUBMIT_SUCCESS,
+  WS_CONNECTION_CLOSE,
+  WS_CONNECTION_CLOSED,
+  WS_CONNECTION_ERROR,
+  WS_CONNECTION_START,
+  WS_CONNECTION_SUCCESS,
+  WS_GET_MESSAGE,
+} from "../constants/index";
+
+export interface IIngredient {
+  _id: string;
+  name: string;
+  type: string;
+  proteins: number;
+  fat: number;
+  carbohydrates: number;
+  calories: number;
+  price: number;
+  image: string;
+  image_mobile: string;
+  image_large: string;
+  __v: number;
+}
+
+export interface IAddIngredient {
+  readonly type: typeof ADD_INGREDIENT;
+  readonly id: string;
+  readonly payload: IIngredient;
+}
+
+export interface ISortIngredients {
+  readonly type: typeof SORT_INGREDIENTS;
+  payload: ReadonlyArray<IIngredient>;
+}
+
+export interface IDeleteIngredient {
+  readonly type: typeof DELETE_INGREDIENT;
+  readonly payload: IIngredient;
+}
+
+export interface IResetIngredient {
+  readonly type: typeof RESET_INGREDIENT;
+}
+
+export type TConstructorAction =
+  | IAddIngredient
+  | ISortIngredients
+  | IDeleteIngredient
+  | IResetIngredient;
+
 export interface IOrder {
   _id: string;
   ingredients: Array<string>;
