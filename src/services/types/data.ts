ccd0b336e1b6f4959c5fe174248f5f274e5dba31
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-06 11:13:48 +0300

    feat: добавить интерфес заказа

--- src/services/types/data.ts
+++ src/services/types/data.ts
@@ -1,1 +1,9 @@
-export {}
+export interface IOrder {
+  _id: string;
+  ingredients: Array<string>;
+  status: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+  number: number;
+}
