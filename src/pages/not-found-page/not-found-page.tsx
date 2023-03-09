2a83eeca7451137591252e2e7eaae01f39ef4b27
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-09 11:18:38 +0300

    feat: добавить типизацию для not-found-page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,11 +1,14 @@
+import { FC } from "react";
 import style from "./not-found-page.module.css";
 
-export default function NotFoundPage() {
+const NotFoundPage: FC = () => {
   return (
-    <>      
+    <>
       <p className={`text text_type_main-large ${style.text}`}>
         Такой страницы не существует
       </p>
     </>
   );
-}
+};
+
+export default NotFoundPage;
